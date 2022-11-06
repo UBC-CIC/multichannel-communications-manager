2366dd440c0f3f28c95b8dc55688e153602a91ff
Fu <mminting@ead.ubc.ca>
2022-11-06 14:58:08 -0800

    write sql create statements

---
+++ queries.sql
@@ -0,0 +1,37 @@
+CREATE TABLE User (
+    user_id int PRIMARY KEY AUTO_INCREMENT, 
+    email_address text UNIQUE NOT NULL INDEX,
+    phone_address int UNIQUE,
+    phone_id int UNIQUE AUTO_INCREMENT,
+    postal_code varchar(10),
+    province ENUM('AB','BC','MB','NB','NL','NT','NS','NU','ON','PE','QC','SK','YT') NOT NULL
+)
+
+CREATE TABLE Category (
+    category_id int PRIMARY KEY AUTO_INCREMENT,
+    acronym varchar(10) UNIQUE NOT NULL,
+    title varchar(50) NOT NULL,
+    description text 
+)
+
+CREATE TABLE Topic (
+    topic_id int PRIMARY KEY AUTO_INCREMENT,
+    acronym varchar(10) UNIQUE, NOT NULL
+)
+
+CREATE TABLE CategoryTopic (
+    categoryTopic_id int PRIMARY KEY AUTO_INCREMENT,
+    category_id int NOT NULL FOREIGN KEY REFERENCES Category(category_id),
+    topic_id int NOT NULL FOREIGN KEY REFERENCES Topic(topic_id)
+)
+
+CREATE TABLE UserCategoryTopic (
+    user_id int NOT NULL FOREIGN KEY REFERENCES User(user_id),
+    categoryTopic_id int NOT NULL FOREIGN KEY REFERENCES CategoryTopic(categoryTopic_id),
+    email_notice boolean NOT NULL,
+    sms notice boolean NOT NULL
+)
+
+-- TODO: create index
+-- CREATE INDEX user_email ON User(email_address)
+CREATE UNIQUE INDEX category_topic on CategoryTopic(category_id, topic_id)
