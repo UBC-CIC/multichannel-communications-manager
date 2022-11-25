3c4ab8e42bc6bb6451b25a0397b696eacfcf08da
Fu <mminting@ead.ubc.ca>
2022-11-25 10:22:20 -0800

    regenerate queries in src folder

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,194 +1,101 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getUser = /* GraphQL */ `
-  query GetUser($email: String!) {
-    getUser(email: $email) {
-      id
-      email
-      phoneNumber
-      phoneId
+export const getUserById = /* GraphQL */ `
+  query GetUserById($user_id: Int!) {
+    getUserById(user_id: $user_id) {
+      user_id
+      email_address
+      phone_address
+      postal_code
       province
-      postalCode
-      follows {
-        items {
-          id
-          emailNotice
-          textNotice
-          createdAt
-          updatedAt
-          userFollowsId
-          topicFollowsId
-          owner
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      owner
     }
   }
 `;
-export const listUsers = /* GraphQL */ `
-  query ListUsers(
-    $email: String
-    $filter: ModelUserFilterInput
-    $limit: Int
-    $nextToken: String
-    $sortDirection: ModelSortDirection
-  ) {
-    listUsers(
-      email: $email
-      filter: $filter
-      limit: $limit
-      nextToken: $nextToken
-      sortDirection: $sortDirection
-    ) {
-      items {
-        id
-        email
-        phoneNumber
-        phoneId
-        province
-        postalCode
-        follows {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        owner
-      }
-      nextToken
+export const getUserByEmail = /* GraphQL */ `
+  query GetUserByEmail($user_email: String!) {
+    getUserByEmail(user_email: $user_email) {
+      user_id
+      email_address
+      phone_address
+      postal_code
+      province
     }
   }
 `;
-export const getTopic = /* GraphQL */ `
-  query GetTopic($id: ID!) {
-    getTopic(id: $id) {
-      id
+export const getUserCategoryTopicByUserId = /* GraphQL */ `
+  query GetUserCategoryTopicByUserId($user_id: Int!) {
+    getUserCategoryTopicByUserId(user_id: $user_id) {
+      categoryTopic_id
+      category_acronym
+      topic_acronym
+    }
+  }
+`;
+export const getCategoryTopicById = /* GraphQL */ `
+  query GetCategoryTopicById($categoryTopic_id: Int!) {
+    getCategoryTopicById(categoryTopic_id: $categoryTopic_id) {
+      categoryTopic_id
+      category_acronym
+      topic_acronym
+    }
+  }
+`;
+export const getCategoryByAcronym = /* GraphQL */ `
+  query GetCategoryByAcronym($acronym: String!) {
+    getCategoryByAcronym(acronym: $acronym) {
+      category_id
+      acronym
       title
       description
-      follows {
-        items {
-          id
-          emailNotice
-          textNotice
-          createdAt
-          updatedAt
-          userFollowsId
-          topicFollowsId
-          owner
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
     }
   }
 `;
-export const listTopics = /* GraphQL */ `
-  query ListTopics(
-    $id: ID
-    $filter: ModelTopicFilterInput
-    $limit: Int
-    $nextToken: String
-    $sortDirection: ModelSortDirection
-  ) {
-    listTopics(
-      id: $id
-      filter: $filter
-      limit: $limit
-      nextToken: $nextToken
-      sortDirection: $sortDirection
-    ) {
-      items {
-        id
-        title
-        description
-        follows {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      nextToken
+export const getTopicsOfCategoryByAcronym = /* GraphQL */ `
+  query GetTopicsOfCategoryByAcronym($category_acronym: String!) {
+    getTopicsOfCategoryByAcronym(category_acronym: $category_acronym) {
+      topic_id
+      acronym
     }
   }
 `;
-export const getFollow = /* GraphQL */ `
-  query GetFollow($id: ID!) {
-    getFollow(id: $id) {
-      id
-      user {
-        id
-        email
-        phoneNumber
-        phoneId
-        province
-        postalCode
-        follows {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        owner
-      }
-      topic {
-        id
-        title
-        description
-        follows {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      emailNotice
-      textNotice
-      createdAt
-      updatedAt
-      userFollowsId
-      topicFollowsId
-      owner
+export const getTopicByAcronym = /* GraphQL */ `
+  query GetTopicByAcronym($topic_acronym: String!) {
+    getTopicByAcronym(topic_acronym: $topic_acronym) {
+      topic_id
+      acronym
     }
   }
 `;
-export const listFollows = /* GraphQL */ `
-  query ListFollows(
-    $filter: ModelFollowFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listFollows(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        user {
-          id
-          email
-          phoneNumber
-          phoneId
-          province
-          postalCode
-          createdAt
-          updatedAt
-          owner
-        }
-        topic {
-          id
-          title
-          description
-          createdAt
-          updatedAt
-        }
-        emailNotice
-        textNotice
-        createdAt
-        updatedAt
-        userFollowsId
-        topicFollowsId
-        owner
-      }
-      nextToken
+export const getAllCategories = /* GraphQL */ `
+  query GetAllCategories {
+    getAllCategories {
+      category_id
+      acronym
+      title
+      description
     }
+  }
+`;
+export const getAllTopics = /* GraphQL */ `
+  query GetAllTopics {
+    getAllTopics {
+      topic_id
+      acronym
+    }
+  }
+`;
+export const getAllCategoryTopics = /* GraphQL */ `
+  query GetAllCategoryTopics {
+    getAllCategoryTopics {
+      categoryTopic_id
+      category_acronym
+      topic_acronym
+    }
+  }
+`;
+export const testSQL = /* GraphQL */ `
+  query TestSQL {
+    testSQL
   }
 `;
